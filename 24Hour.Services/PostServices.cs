bacaa89f59e9aeff352bc4e4cce5216dbfc8cbd1
mmajors1 <mmajors1@mac.com>
2020-10-24 00:18:20 -0400

    pushing updated dev branch

---
+++ 24Hour.Services/PostServices.cs
@@ -0,0 +1,57 @@
+﻿using _24Hour.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Services
+{
+    public class PostServices
+    {
+        private readonly Guid _userId;
+
+        public PostServices(Guid userId)
+        {
+            _userId = userId;
+        }
+    }
+    public bool CreatePost(PostCreate model)
+    {
+        var entity =
+            new Post)
+            {
+                OwnerId = _userId,
+                Title = model.Title,
+                Content = model.Content,
+                CreatedUtc = DateTimeOffset.Now
+            };
+
+        using (var ctx = new ApplicationDbContext())
+        {
+            ctx.Post.Add(entity);
+            return ctx.SaveChanges() == 1;
+        }
+    }
+    public IEnumerable<PostListItem> GetPosts()
+    {
+        using (var ctx = new ApplicationDbContext())
+        {
+            var query =
+                ctx
+                    .Posts
+                    .Where(e => e.OwnerId == _userId)
+                    .Select(
+                        e =>
+                            new PostListItem
+                            {
+                                NoteId = e.NoteId,
+                                Title = e.Title,
+                                CreatedUtc = e.CreatedUtc
+                            }
+                    );
+
+            return query.ToArray();
+        }
+    }
+}
---
+++ 24Hour.WebAPI/Controllers/PostService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _24Hour.WebAPI.Controllers
+{
+    internal class PostService
+    {
+        private Guid userId;
+
+        public PostService(Guid userId)
+        {
+            this.userId = userId;
+        }
+    }
+}
